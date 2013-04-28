b21d0a8625dde7bfa8e218967638c3ff8541e438
dirk <dirkmoermans@gmail.com>
2013-04-28 21:42:56 +0200

    use constructor forwarding

--- cheapshot/material.hh
+++ cheapshot/material.hh
@@ -19,13 +19,6 @@ namespace cheapshot
       struct scoped_material
       {
       public:
-         scoped_material(Controller& mat_cont_,side c,piece p):
-            mat_cont(mat_cont_),
-            old_material(mat_cont.material)
-         {
-            mat_cont.material-=score::weight(c,p);
-         }
-
          template<typename Op, typename... Args>
          scoped_material(Controller& mat_cont_,const Op& op,  Args&&...  args):
             mat_cont(mat_cont_),
@@ -33,6 +26,10 @@ namespace cheapshot
          {
             mat_cont.material-=op(std::forward<Args>(args)...);
          }
+
+         scoped_material(Controller& mat_cont_,side c,piece p):
+            scoped_material(mat_cont_,(int (*)(side,piece))score::weight,c,p)
+         {}
 
          ~scoped_material()
          {
@@ -47,17 +44,21 @@ namespace cheapshot
 
       struct noop_material
       {
-         noop_material(const board_t& b){}
+         noop_material(const board_t& b)
+         {}
+
          static constexpr int material=0;
       };
 
       template<>
       struct scoped_material<noop_material>
       {
-         scoped_material(noop_material& mat_cont_, side c, piece p){}
+         scoped_material(noop_material& mat_cont_, side c, piece p)
+         {}
 
          template<typename Op, typename... Args>
-         scoped_material(noop_material& mat_cont_,const Op& op,  Args&&...  args){}
+         scoped_material(noop_material& mat_cont_,const Op& op,  Args&&...  args)
+         {}
 
          scoped_material(const scoped_material&) = delete;
          scoped_material& operator=(const scoped_material&) = delete;
